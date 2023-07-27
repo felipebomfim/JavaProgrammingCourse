ac1733ca1780998e707eac398c3c0ef86b9ab54d
Felipe Bomfim <felipebomfim4161@gmail.com>
2023-07-26 23:00:25 -0300

    First Java Project of a Multiplication Table

---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,14 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable multTable = new MultiplicationTable();
+
+		multTable.print();
+		multTable.print(6);
+		multTable.print(6, 11, 20);
+
+	}
+
+}
