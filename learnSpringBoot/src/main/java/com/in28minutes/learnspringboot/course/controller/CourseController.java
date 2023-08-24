88ee3c4092ec70124f6e3b7a31bccf19e204168b
felipebomfim <felipebomfim4161@gmail.com>
2023-08-24 00:15:36 -0300

    Course completed

--- learnSpringBoot/src/main/java/com/in28minutes/learnspringboot/course/controller/CourseController.java
+++ learnSpringBoot/src/main/java/com/in28minutes/learnspringboot/course/controller/CourseController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.learnspringboot.course.bean.Course;
@@ -29,10 +34,31 @@ public class CourseController {
 	}
 	
 	@GetMapping("/courses/{id}")
-	public Optional<Course> getCourse(@PathVariable long id){
+	public Course getCourse(@PathVariable long id){
 //		Optional<Course> course = courses.stream().filter(c -> c.getId() == id).findFirst();
 		Optional<Course> course = courseRepository.findById(id);
-	    return course;
+		
+		if (course.isEmpty()){
+			throw new RuntimeException("Course not found with id"+ id);
+		}
+		
+	    return course.get();
+	}
+	
+	@PostMapping("/courses")
+	public void createCourse(@RequestBody Course course){
+		courseRepository.save(course);
+	}
+	
+	@PutMapping("/courses/{id}")
+	public void updateCourse(@PathVariable long id, @RequestBody Course course) {
+		courseRepository.save(course);
 	}
 
+	@DeleteMapping("/courses/{id}")
+	public void deleteCourse(@PathVariable long id) {
+		courseRepository.deleteById(id);
+	}
+
+	
 }
